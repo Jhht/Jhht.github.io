7ecaee13fe9c1649679f0f21aad660b8c59dbbf4
Juan Ferreiro Fernandez <jferreiro@Juans-MacBook-Air.local>
2024-07-23 23:50:24 +0200

    App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './Chat';
+import Header from './Header';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Chat />
+        </div>
+    );
+};
+
+export default App;
